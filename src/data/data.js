b9c81b4e2f89a75e483ff073093a414fb32ddc06
HossainRafi <hossainrafi71@gmail.com>
2022-11-20 21:35:25 +0600

    footer data added

--- src/data/data.js
+++ src/data/data.js
@@ -340,11 +340,46 @@ const story = {
     },
   ],
 };
+
+const footerAPI = {
+  titles: [
+    { title: "About Nike" },
+    { title: "Get Help" },
+    { title: "Company" },
+  ],
+  links: [
+    [
+      { link: "News" },
+      { link: "Careers" },
+      { link: "Investors" },
+      { link: "Prupose" },
+      { link: "Sustainability" },
+    ],
+    [
+      { link: "Order Status" },
+      { link: "Shipping & Delivery" },
+      { link: "Payment Options" },
+      { link: "Gift Card Balance" },
+      { link: "Contact Us" },
+      { link: "FAQ" },
+      { link: "Blog" },
+    ],
+    [
+      { link: "Gift Cards" },
+      { link: "Promotions" },
+      { link: "Find A Store" },
+      { link: "Signup" },
+      { link: "Nike Jouneral" },
+      { link: "Send Us Feeback" },
+    ],
+  ],
+};
+
 export {
-  heroapi,
-  popularsales,
+  heroapi,popularsales,
   sneaker,
   highlight,
   toprateslaes,
   story,
+  footerAPI,
 };
