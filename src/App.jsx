668e542072532055b3ab8c801260263440fc76ee
HossainRafi <hossainrafi71@gmail.com>
2022-11-20 19:51:40 +0600

    hero structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div>
-      <h1>welcomeertertgertgertge</h1>
-    </div>
+    <>
+      <main>
+        <Hero/>
+      </main>
+    </>
   );
 };
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <div>
+          <h1></h1>
+          <h1></h1>
+          <button></button>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <img src="" className=""/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
