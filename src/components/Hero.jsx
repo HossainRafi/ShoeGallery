74eb1753bb75d56ef861c296eb6d0ea787806742
HossainRafi <hossainrafi71@gmail.com>
2022-11-20 22:16:19 +0600

    button and image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,15 +11,23 @@ const Hero = ({
           <h1 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200">
             {title}
           </h1>
-          <h1 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200">
+          <h1 className="text-6xl mt-2 lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200">
             {subtitle}
           </h1>
-          <button>{btntext}</button>
+          <button
+            type="button"
+            className="button-theme bg-slate-200  shadow-slate-200 rounded-xl my-5"
+          >
+            {btntext}
+          </button>
           {/* <div></div>
           <div></div> */}
         </div>
-        <div className="">
-          <img src={img} className="" />
+        <div className="flex items-center mt-5">
+          <img
+            src={img}
+            className="w-auto h-[45vh] lg:h-[35vh] md:h-[31vh] sm:h-[21vh] xsm:h-[19vh] transitions-theme -rotate-[25deg] hover:rotate-0 cursor-pointer object-fill"
+          />
         </div>
       </div>
     </div>
