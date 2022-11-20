c883df8558850852eca77cb509a7c9ebdd93b701
HossainRafi <hossainrafi71@gmail.com>
2022-11-20 23:39:44 +0600

    video commented

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,11 +24,11 @@ const Hero = ({
           </button>
 
           {/* ====================================== */}
-          <div className="grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto">
+          {/* <div className="grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto">
             {videos?.map((val, i) => (
               <Clips key={i} imgsrc={val.imgsrc} clip={val.clip} />
             ))}
-          </div>
+          </div> */}
           {/* ====================================== */}
 
           <div className="grid items-center absolute top-[33vh] lg:top-[27vh] right-0 gap-3">
