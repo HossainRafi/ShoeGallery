9e16f06f520b52ce057b535376540294426092bb
HossainRafi <hossainrafi71@gmail.com>
2022-11-20 22:01:01 +0600

    heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,14 @@ const Hero = ({
   return (
     <div className="relative h-auto w-auto flex flex-col">
       <div className="bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10"></div>
-      <div className="">
-        <div className="">
-          <h1>{title}</h1>
-          <h1>{subtitle}</h1>
+      <div className="relative opacity-100 z-20 grid items-center justify-items-center nike-container">
+        <div className="grid items-center justify-items-center mt-28 md:mt-24">
+          <h1 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200">
+            {title}
+          </h1>
+          <h1 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200">
+            {subtitle}
+          </h1>
           <button>{btntext}</button>
           {/* <div></div>
           <div></div> */}
