52c60f8ba2620b2f5342e593d9bc2753d29bde88
HossainRafi <hossainrafi71@gmail.com>
2022-11-20 20:37:50 +0600

    popular sales data added

--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,22 @@
 import heroimg from "../assets/hero.png";
 
+import hightlightimg from "../assets/hightlightimg.png";
+import sneakershoe from "../assets/sneaker.png";
+
 import clip from "../assets/video/clip.mp4";
 import vcover1 from "../assets/video/vcover1.png";
 import vcover2 from "../assets/video/vcover2.png";
 import vcover3 from "../assets/video/vcover3.png";
+
+import psale1 from "../assets/nike-air-red.png";
+import psale2 from "../assets/nike-adapt-bb.png";
+import psale3 from "../assets/nike-adapt-bb-smart.png";
+
+import facebook from "../assets/facebook.svg";
+import instagram from "../assets/instagram.svg";
+import twitter from "../assets/twitter.svg";
+import youtube from "../assets/youtube.svg";
+import messenger from "../assets/messenger.svg";
 
 const heroapi = {
   title: "Play With Electric Nike",
@@ -24,4 +37,46 @@ const heroapi = {
   ],
 };
 
-export { heroapi };
+const popularsales = {
+  title: "Popular Sales",
+  items: [
+    {
+      id: "0p0x1",
+      title: "Nike Addapt BB 2.0",
+      text: "MEN Running Shoes",
+      rating: "4.9",
+      btn: "Buy Now",
+      img: psale2,
+      price: "200",
+      color: "from-blue-600 to-blue-500",
+      shadow: "shadow-lg shadow-blue-500",
+    },
+    {
+      id: "0p0x2",
+      title: "Nike Martine Rose",
+      text: "MEN Running Shoes",
+      rating: "4.5",
+      btn: "Buy Now",
+      img: psale1,
+      price: "200",
+      color: "from-red-500 to-rose-500",
+      shadow: "shadow-lg shadow-rose-500",
+    },
+    {
+      id: "0p0x3",
+      title: "Nike Smart Shoe 2.0",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: psale3,
+      price: "200",
+      color: "from-violet-500 to-indigo-500",
+      shadow: "shadow-lg shadow-violet-500",
+    },
+  ],
+};
+
+export {
+  heroapi,
+  popularsales,
+};
