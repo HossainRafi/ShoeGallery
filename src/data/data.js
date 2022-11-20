3fd50abd9d698abee84451d71441b5481746e828
HossainRafi <hossainrafi71@gmail.com>
2022-11-20 20:10:16 +0600

    hero data added

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+import heroimg from "../assets/hero.png";
+
+import clip from "../assets/video/clip.mp4";
+import vcover1 from "../assets/video/vcover1.png";
+import vcover2 from "../assets/video/vcover2.png";
+import vcover3 from "../assets/video/vcover3.png";
+
+const heroapi = {
+  title: "Play With Electric Nike",
+  subtitle: "Adapt 2.0 Sneakers",
+  img: heroimg,
+  btntext: "Explore Product",
+  videos: [
+    { imgsrc: vcover1, clip: clip },
+    { imgsrc: vcover2, clip: clip },
+    { imgsrc: vcover3, clip: clip },
+  ],
+  sociallinks: [
+    { icon: facebook },
+    { icon: messenger },
+    { icon: instagram },
+    { icon: twitter },
+    { icon: youtube },
+  ],
+};
+
+export { heroapi };
