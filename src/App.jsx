4f9019789562d58fef577c91f5ec85c4ba228601
HossainRafi <hossainrafi71@gmail.com>
2022-11-21 20:19:44 +0600

    destructurnig data in flex content component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { FlexContent, Hero, Sales } from "./components";
-import { heroapi, popularsales, toprateslaes } from "./data/data.js";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+} from "./data/data.js";
 
 const App = () => {
   return (
@@ -8,8 +14,9 @@ const App = () => {
       <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} />
         <Sales endpoint={toprateslaes} />
-        <FlexContent />
+        <FlexContent endpoint={sneaker} />
       </main>
     </>
   );
--- src/components/FlexContent.jsx
+++ src/components/FlexContent.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
-const FlexContent = () => {
+const FlexContent = ({
+  ifExists,
+  endpoint: { title, heading, text, img, btn, url },
+}) => {
   return (
     <>
       <div>
         <div>
-          <h1></h1>
-          <h1></h1>
-          <p></p>
-          <a>
-            <button type="button" className=""></button>
+                  <h1>{heading}</h1>
+                  <h1>{title}</h1>
+                  <p>{text}</p>
+          <a href={url}>
+                      <button type="button" className="">{btn}</button>
           </a>
         </div>
         <div>
-          <img />
+          <img src={img} />
         </div>
       </div>
     </>
