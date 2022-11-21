d5703ba9fbd8f3d4838907f721ea2e73216e23d9
HossainRafi <hossainrafi71@gmail.com>
2022-11-21 18:29:40 +0600

    items components structure done

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { ShoppingBagIcon, StarIcon } from "@heroicons/react/24/solid";
+import React from "react";
 
-const Item = () => {
+const Item = ({
+  ifExists,
+  id,
+  color,
+  shadow,
+  title,
+  text,
+  img,
+  btn,
+  rating,
+  price,
+}) => {
   return (
-    <div>Item</div>
-  )
-}
+    <>
+      <div>
+        <div>
+          <h1>{title}</h1>
+          <p>{text}</p>
+          <div>
+            <div>
+              <h1>{price}</h1>
+            </div>
+            <div>
+              <StarIcon className="icon-style w-5 h-5 md:w-4 md:h-4" />
+              <h1>{rating}</h1>
+            </div>
+          </div>
+          <div>
+            <button type="button" className="">
+              <ShoppingBagIcon className="icon-style text-slate-900" />
+            </button>
+            <button type="button" className="">
+              {btn}
+            </button>
+          </div>
+        </div>
+        <div>
+          <img src={img} className="" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Item
+export default Item;
