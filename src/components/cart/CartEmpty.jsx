7baa31fb38414beba9bf4474d03a877f17f4d5ca
HossainRafi <hossainrafi71@gmail.com>
2022-11-22 15:40:30 +0600

    empty cart style

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import emptybag from "../../assets/emptybag.png";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 const CartEmpty = () => {
   return (
     <>
-      <div></div>
+      <div className="flex items-center justify-center flex-col h-screen px-11 text-center gap-7">
+        <img
+          src={emptybag}
+          alt="emptybag/img"
+        />
+        <button
+          type="button"
+        >
+          <ArrowLeftIcon className="w-5 h-5 text-slate-900" />
+          <span className="">Back To Nike Store</span>
+        </button>
+      </div>
     </>
   );
 };
