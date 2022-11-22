a33c521d2123fd38b56e3665cdc3a592aa4889a7
HossainRafi <hossainrafi71@gmail.com>
2022-11-22 11:17:26 +0600

    header style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   HeartIcon,
   MagnifyingGlassIcon,
@@ -9,21 +8,27 @@ import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
     <>
-      <header>
-        <nav>
-          <div>
-            <img src={logo} className="" />
+      <header className="fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[200] blur-effect-theme">
+        <nav className="flex items-center justify-between nike-container">
+          <div className="flex items-center">
+            <img src={logo} alt="logo/img" className="" />
           </div>
-          <ul>
-            <li>
-              <MagnifyingGlassIcon />
+          <ul className="flex items-center justify-center gap-2">
+            <li className="grid items-center">
+              <MagnifyingGlassIcon className="" />
             </li>
-            <li>
-              <HeartIcon />
+            <li className="grid items-center">
+              <HeartIcon className="" />
             </li>
-            <li>
-              <button type="button">
-                <ShoppingBagIcon />
+            <li className="grid items-center">
+              <button
+                type="button"
+                className="border-none outline-none active:scale-110 transition-all duration-300 relative"
+              >
+                <ShoppingBagIcon className="" />
+                <div className="absolute top-4 right-0 shadow w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300">
+                  0
+                </div>
               </button>
             </li>
           </ul>
