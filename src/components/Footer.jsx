9093be9816b09d974608439e71c8022660ada93b
HossainRafi <hossainrafi71@gmail.com>
2022-11-22 10:43:37 +0600

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = ({ footerAPI: { titles, links } }) => {
   }, []);
   return (
     <>
-      <footer>
-        <div>
-          <div>
+      <footer className="bg-theme pt-7 pb-5">
+        <div className="nike-container text-slate-200">
+          <div className="grid items-start grid-cols-3 max-w-2xl w-full m-auto md:max-w-none md:gap-5">
             {titles.map((val, i) => (
-              <div key={i}>
-                <h1>
+              <div key={i} className="grid items-center">
+                <h1 className="text-lg lg:text-base md:text-sm uppercase font-semibold">
                   {val.title}
                 </h1>
               </div>
@@ -28,11 +28,11 @@ const Footer = ({ footerAPI: { titles, links } }) => {
               </ul>
             ))}
           </div>
-          <div >
-            <p >
-              Copyright<sup >&copy;</sup> All
+          <div className="mt-5 text-center">
+            <p className="text-sm md:text-center">
+              Copyright<sup className="text-base font-bold">&copy;</sup> All
               Reserved Rights{" "}
-              <span>JSSTACK DEVELOPERS {Year}</span>
+              <span className="font-semibold">HOSSAIN RAFI {Year}</span>
             </p>
           </div>
         </div>
