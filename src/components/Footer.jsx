6dc12b114421fd1ee8c3de5e9b8aa6f6a2175e34
HossainRafi <hossainrafi71@gmail.com>
2022-11-22 10:31:47 +0600

    footer structure done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Footer = ({ footerAPI: { titles, links } }) => {
+  
   return (
     <>
-    <Footer>
-              
-      </Footer>
+      <footer>
+        <div>
+          <div>
+            {titles.map((val, i) => (
+              <div key={i}>
+                <h1>
+                  {val.title}
+                </h1>
+              </div>
+            ))}
+            {links.map((list, i) => (
+              <ul key={i}>
+                {list.map((link, i) => (
+                  <li key={i}>
+                    {link.link}
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+          <div>
+            <p>
+              Copyright<sup>&copy;</sup> All
+              Reserved Rights{" "}
+              <span>JSSTACK DEVELOPERS {Year}</span>
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
