afe4ab4971ec2919d493a4639b9d0a0830c013ea
HossainRafi <hossainrafi71@gmail.com>
2022-11-22 12:49:21 +0600

    design of cart div

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,8 +6,12 @@ import CartItem from "./cart/CartItem";
 const Cart = () => {
   return (
     <>
-      <div>
-        <div>
+      <div
+        className={`fixed top-0 left-0 right-0 bottom-0 blur-effect-theme duration-500 w-full h-screen opacity-100 z-[250]`}
+      >
+        <div
+          className={`blur-effect-theme duration-500 h-screen max-w-xl w-full absolute right-0`}
+        >
           <CartCount />
           <CartEmpty />
           <CartItem />
